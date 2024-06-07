b1d99a680d7247534b86ef7d791c2a6ae6daf9e9
Rumit Singh Tuteja <rtuteja@i-360.com>
2024-06-07 14:59:09 -0400

    Initial Commit

---
+++ PhotoSaver/PhotoSaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoSaverApp.swift
+//  PhotoSaver
+//
+//  Created by Rumit Singh Tuteja on 6/7/24.
+//
+
+import SwiftUI
+
+@main
+struct PhotoSaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
